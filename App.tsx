d70f9867f556c8e7b8f5b2ac5e84afa1145fe466
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-01-17 22:34:12 +0900

    feat: UnfinishedScreenを追加

--- App.tsx
+++ App.tsx
@@ -1,71 +1,15 @@
+import { UnfinishedScreen } from '@/screens';
 import { StatusBar } from 'expo-status-bar';
-import { useCallback, useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import DraggableFlatList, {
-  ScaleDecorator,
-  ShadowDecorator,
-  OpacityDecorator,
-  RenderItemParams,
-} from "react-native-draggable-flatlist";
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { mapIndexToData, Item } from "@/utils";
-
-const NUM_ITEMS = 100;
-
-const initialData: Item[] = [...Array(NUM_ITEMS)].map(mapIndexToData);
-
 export default function App() {
-  const [data, setData] = useState(initialData);
-
-  const renderItem = useCallback(
-    ({ item, drag, isActive }: RenderItemParams<Item>) => {
-      return (
-        <ShadowDecorator>
-          <ScaleDecorator>
-            <OpacityDecorator>
-              <TouchableOpacity
-                activeOpacity={1}
-                onLongPress={drag}
-                disabled={isActive}
-                style={[
-                  styles.rowItem,
-                  { backgroundColor: isActive ? "blue" : item.backgroundColor },
-                ]}
-              >
-                <Text style={styles.text}>{item.text}</Text>
-              </TouchableOpacity>
-            </OpacityDecorator>
-          </ScaleDecorator>
-        </ShadowDecorator>
-      );
-    },
-    []
-  );
-
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <StatusBar style="auto" />
-        <DraggableFlatList
-          data={data}
-          onDragEnd={({ data }) => setData(data)}
-          keyExtractor={(item) => item.key}
-          renderItem={renderItem}
-          renderPlaceholder={() => (
-            <View style={{ flex: 1, backgroundColor: "tomato" }} />
-          )}
-        />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={StyleSheet.absoluteFill}>
+      <SafeAreaProvider>
+        <UnfinishedScreen />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/index.ts
@@ -0,0 +1,1 @@
+export {UnfinishedScreen} from './UnfinishedScreen'
