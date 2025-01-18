34210b8b3577728aacaed9f0c440e5aaeb3f031a
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-01-18 22:30:52 +0900

    feat: IconButtonを追加

--- src/screens/TodoScreen.tsx
+++ src/screens/TodoScreen.tsx
@@ -3,6 +3,7 @@ import { FlashList } from '@shopify/flash-list';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+import { IconButton } from 'react-native-paper';
 
 const DATA = [
     { title: "First Item" },
@@ -41,11 +42,21 @@ export const TodoScreen = () => {
     return (
         <SafeAreaView style={styles.safearea}>
             <StatusBar style="auto" />
-            <Header />
-            <FlashList
-                data={DATA}
-                renderItem={({ item }) => <Item title={item.title} />} // renderItemでItemコンポーネントを使用
-                estimatedItemSize={200}
+            <View style={styles.container}>
+                <Header />
+                <FlashList
+                    data={DATA}
+                    renderItem={({ item }) => <Item title={item.title} />} // renderItemでItemコンポーネントを使用
+                    estimatedItemSize={200}
+                />
+            </View>
+            <IconButton
+                containerColor={'#3b82f6'}
+                icon="plus"
+                iconColor={"white"}
+                size={50}
+                style={styles.addButton}
+                onPress={() => console.log('Pressed')}
             />
         </SafeAreaView>
     );
@@ -56,11 +67,20 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         flex: 1
     },
+    container:{
+        backgroundColor: 'white',
+        flex: 1
+    },
     item: {
         borderBottomColor: '#d1d5db',
         borderBottomWidth: 1,
         borderStyle: 'solid',
         flex: 1,
         height: 70,
+    },
+    addButton: {
+        bottom: 20,
+        right: 20,
+        position: 'absolute'
     }
 });
