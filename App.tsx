f8fa302a36f8f41d5cc966c47821aeb5baab8855
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-01-20 00:52:31 +0900

    fix: TodoProviderを削除

--- App.tsx
+++ App.tsx
@@ -5,19 +5,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 
 import { TodoScreen } from '@/screens';
-import { TodoProvider } from '@/context';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={StyleSheet.absoluteFill}>
-      <TodoProvider>
         <NavigationContainer>
           <SafeAreaProvider>
             <StatusBar style="auto" />
             <TodoScreen />
           </SafeAreaProvider>
         </NavigationContainer>
-      </TodoProvider>
     </GestureHandlerRootView>
   );
 }
