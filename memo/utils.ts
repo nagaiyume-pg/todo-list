493e56e5b99523f40a32a07b676967f515d02bc9
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-01-23 02:00:27 +0900

    delete: memo

--- memo/utils.ts
+++
@@ -1,9 +0,0 @@
-export const mapIndexToData = (_d: any, index: number, arr: any[]) => {
-    return {
-        text: 'タスク',
-        key: `key-${index}`,
-        height: 70,
-    };
-};
-
-export type Item = ReturnType<typeof mapIndexToData>;
