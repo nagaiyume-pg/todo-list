c834636adf72c090e28eb587b30a5ed271dbe5e4
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-01-23 18:46:10 +0900

    feat: TabViewを途中まで作成

--- src/components/TabView.tsx
+++ src/components/TabView.tsx
@@ -2,41 +2,59 @@ import { Icon } from "@rneui/themed"
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 
 const Tab = () => {
-    return(
-        <TouchableOpacity>
-            <View>
-                <Icon name="checkbox-blank-outline" color={color} type='material-community' size={20} />
-                <Text>未完了</Text>
-            </View>
-        </TouchableOpacity>
-    )
+    return (
+        <View style={styles.tab}>
+            <TouchableOpacity style={styles.labelButton}>
+                <View style={styles.label}>
+                    <Icon name="checkbox-blank-outline" color={'red'} type='material-community' size={20} />
+                    <Text style={styles.labelText}>未完了</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
-const TabBar = () => {
-    return(
-        <View style={styles.tabBar}>
-            <Tab></Tab>
-            <Tab></Tab>
+const Tabbar = () => {
+    return (
+        <View style={styles.tabbar}>
+            <Tab /><Tab />
         </View>
-    )
+    );
 }
 
 export const TabView = () => {
-    return(
-        <TabBar />
+    return (
+        <Tabbar />
     );
 }
 
 const styles = StyleSheet.create({
-    tabBar: {
-        backgroundColor: 'red',
+    tabbar: {
+        backgroundColor: 'white',
         borderBlockColor: '#d1d5db',
         borderBottomWidth: 1,
         borderStyle: 'solid',
         display: 'flex',
-        height: 30
+        flexDirection: 'row',
+        height: 30,
     },
     tab: {
-        height: 30
-    }
-})
+        backgroundColor: 'blue',
+        flex: 1,
+        height: 30,
+    },
+    labelButton: {
+        backgroundColor: 'yellow',
+        flexDirection: 'row', // アイコンとテキストを横並びにする
+        alignItems: 'center', // アイコンとテキストを縦方向に中央揃え
+        paddingHorizontal: 10, // アイコンとテキストにパディングを追加
+    },
+    label: {
+        flexDirection: 'row',
+        alignItems: 'center', // アイコンとテキストを縦方向に中央揃え
+        gap: 5, // アイコンとテキストの間隔
+    },
+    labelText: {
+        fontSize: 14,
+    },
+});
