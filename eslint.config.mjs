e0fa9b1520e6f839180ddb439ea0bf3a39d03fbd
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-02-24 01:22:28 +0900

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,22 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    "react/react-in-jsx-scope": "off",
+  },
+  // Add Prettier plugin and config
+  "plugin:prettier/recommended",
+  {
+    // Optionally, disable some ESLint rules that Prettier handles
+    rules: {
+      "prettier/prettier": ["error"],
+    },
+  },
 ];
