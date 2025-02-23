0f961356a382fabea37ce4f690860f1b30d7eab5
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-02-23 20:00:07 +0900

    feat: TodoItem.storiesを作成

--- src/components/TodoItem/TodoItem.stories.tsx
+++ src/components/TodoItem/TodoItem.stories.tsx
@@ -2,12 +2,17 @@ import { View } from 'react-native';
 import type { Meta, StoryObj } from '@storybook/react';
 import { TodoItem } from './TodoItem';
 
-const meta = {
+const meta: Meta<typeof TodoItem> = {
   title: 'TodoItem',
   component: TodoItem,
   args: {
+    id: "0",
     title: 'タスク名',
+    checked: false,
     width: 375,
+    onCheck: (id: string) => { console.log("Checked Todo with id:", id); },
+    onEdit: (id: string) => { console.log("Edit Todo with id:", id); },
+    onDelete: (id: string) => { console.log("Delete Todo with id:", id); },
   },
   decorators: [
     (Story) => (
@@ -23,7 +28,7 @@ const meta = {
       </View>
     ),
   ],
-} satisfies Meta<typeof TodoItem>;
+};
 
 export default meta;
 
