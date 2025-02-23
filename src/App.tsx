9864d17b8e51d0351efb91608eb8ee5b82e54294
nagaiyume-pg <na9a1yume.pg@gmail.com>
2025-02-24 00:50:15 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 function App() {
   return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
